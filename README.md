0f90da2cbc209fdf2d2eaa2e2162799ea6671a15
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-02-14 12:56:31 -0200

    Better code organization in two loops and aux functions
    * Apriori returns the biggest and frequent sets of elements.

--- README.md
+++ README.md
@@ -8,6 +8,13 @@ use of the minimum amount of memory.
 
 Under development...
 
+Example:
+
+```clojure
+bigdata-apriori.core> (apriori 0.5 [[1 2 3] [1] [2 3] [2 1] [2] [3 2]])
+{#{2 3} 3}
+```
+
 ## License
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- src/bigdata_apriori/core.clj
+++ src/bigdata_apriori/core.clj
@@ -6,42 +6,54 @@
     )
 
 (defn- inc-item-count
-  "Updates the count of a item k in the map m if k occurs in freq-items. 
-  freq-items is a collection of sets."
-  [freq-items m k]
-  {:pre [(every? set? freq-items) (map? m) (set? k)]}
-  (when (or (nil? freq-items)
+  "Updates the count of a item k in the map m if k occurs in fs. 
+  fs is a collection of sets."
+  [fs m k]
+  {:pre [(every? set? fs) (map? m) (set? k)]}
+  (when (or (empty? fs)
             ;; Equivalent of the function contains? for a list of sets (contains?
             ;; does not work for this case).
-            (->> (filter #(= k %) freq-items) seq))
+            (->> (filter #(= k %) fs) seq))
     (if (contains? m k)
       (update-in m [k] inc)
       (assoc m k 1))))
 
+(defn- dissoc-infreq
+  "Remove the infrequent keys from the given map. The frequency is given by the
+  support value."
+  [m sup]
+  {:pre [(map? m)]}
+  (reduce #(if (< (get %1 %2) sup) (dissoc %1 %2) %1)
+          m
+          (keys m)))
+
+(defn- sets-from-combinatons
+  "Returns a collection of sets representing all possible combinations of n of the
+  given keys. Each set is possible combination."
+  [ks n]
+  (->> (comb/combinations ks n) 
+       (map #(apply s/union %))))
 
 (defn apriori
   "support is a double value, representing the percentage of the baskets.
   data is the list of baskets."
   [support data]
   (let [sup-val (* support (count data))]
-    (loop [[basket & r-baskets] data
-           must-count nil ; coll of sets whose elements are frequent.
-           counter-tab {} ; table with the count of each set of elements.
-           n-set 1] ; number of elements of a set.
-      (if basket
-        (recur r-baskets
-               must-count
-               (reduce #(->> %2 set (inc-item-count must-count %1)) 
-                       counter-tab
-                       (comb/combinations basket n-set))
-               n-set)
-        (let [most-freq (map key (filter #(> (val %) sup-val) counter-tab))
-              freq-sets (->> (comb/combinations most-freq (inc n-set))
-                             (map #(apply s/union %)))]
-          (swank.core/break)
-          (if (seq freq-sets)
-            (recur data 
-                   freq-sets
-                   {}
-                   (inc n-set))
-            most-freq))))))
+    (loop [set-size 1
+           freq-tab nil]
+      (let [freq-sets (sets-from-combinatons (keys freq-tab) set-size) 
+            ctab (loop [[basket & r-baskets] data
+                        counter-tab {} ; table with the count of each set of elements.
+                        ]
+                   (if basket
+                     (recur r-baskets
+                            (reduce #(->> %2 set (inc-item-count freq-sets %1)) 
+                                    counter-tab
+                                    (comb/combinations basket set-size)))
+                     counter-tab))
+            most-freq (dissoc-infreq ctab sup-val)
+            ]
+        (if (seq most-freq)
+          (recur (inc set-size) most-freq)
+          freq-tab)
+        ))))
